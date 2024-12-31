0ede429f92e93ea20ad935ea6b85facaea292729
Duncan Finlayson <duncanfinlayson4@gmail.com>
2024-12-30 21:58:46 -0500

    Added Contact Section, plus added theme toggle for light / dark mode

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
     <div className="flex flex-col md:flex-row gap-5
     md:justify-between md:w-full">
         <h2 className="font-semibold text-[22px] md:text-[40px]
-        md:max-w-[462px]">Open to feedback about the site, career opportunites, or to just saying hi!</h2>
+        md:max-w-[462px]">Open to feedback about the site, career opportunites, or to just to say hi!</h2>
         <div className="flex flex-col gap-5 items-center md:items-end">
             <a href="mailto:duncanfinlayson4@gmail.com" className="self-center md:self-start bg-primary text-white
             p-2.5 rounded flex gap-2.5 items-center text-lg
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import TopBackground from "./components/Hero/TopBackground";
 import Navbar from "./components/Navbar";
 import Sections from "./components/Sections";
+import ThemeToggle from "./components/ThemeToggle";
+import Image from 'next/image'
 
 export default function Home() {
   return (
   <div className="relative overflow-clip" id="home">
     <Sections />
     <Navbar />
+    <ThemeToggle/>
     <TopBackground />
+    <Image src="/bottom_gradient_mobile.svg"
+     alt="Bottom gradient background"
+     width={1024}
+     height={700} className="absolute bottom-0 min-w-[1024px]
+     min-h-[700px] -z-50 md:hidden"/>
+         <Image src="/bottom_gradient.svg"
+     alt="Bottom gradient background"
+     width={1557}
+     height={936} className="absolute -bottom-[175px] left-1/2 -translate-x-1/2 min-w-[1557px]
+     min-h-[936px] -z-50 hidden md:block"/>
   </div>
   );
 }
